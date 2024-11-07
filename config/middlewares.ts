8d24105cc1c444cd69007557333b339bd2f06b2a
pericabartulovic <pericab35@gmail.com>
2024-11-07 21:41:31 +0100

    CORS configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,20 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
+    {
+        name: "strapi::cors",
+        config: {
+            origin: ["https://lejla-raguz.netlify.app", "http://localhost:1337"], // Allowed domains
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+            credentials: true,
+        },
+    },
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
